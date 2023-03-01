29623f93b16fb56ea1db5c001d339d54c3986d9b
DOSSEH Shalom <114688102+DataFinanceWiz@users.noreply.github.com>
2023-03-01 13:15:08 +0100

    remove symbols

--- ip.py
+++ ip.py
@@ -1,4 +1,4 @@
-```import random
+import random
 
 # Generate 20 lists of 10 random IP addresses
 for i in range(20):
@@ -9,4 +9,4 @@ for i in range(20):
         octet3 = str(random.randint(0, 255))
         octet4 = str(random.randint(0, 255))
         ip_list.append(octet1 + "." + octet2 + "." + octet3 + "." + octet4)
-    print(ip_list)```
+    print(ip_list)
